5ea1396ce18dd1b01454b430a6137c3d512cf8c3
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-26 12:27:42 -0800

    Comment: add convenience for replying

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,12 @@ class Comment < ActiveRecord::Base
   belongs_to :author, :class_name => "User"
   belongs_to :post
   belongs_to :parent, :class_name => "Comment", :foreign_key => "parent_comment_id"
+
+  def self.reply_to(comment, user, body)
+    Comment.create!(
+      :body => body,
+      :author_id => user.id,
+      :post_id => comment.post_id,
+      :parent_comment_id => comment.id)
+  end
 end
