c58a810038ac36e4b9cffcefb3ab35589e36c119
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-26 16:44:37 -0800

    User: explain how `joins` can do aggregation

--- app/models/user.rb
+++ app/models/user.rb
@@ -94,37 +94,52 @@ class User < ActiveRecord::Base
   end
 
   def joins_post_comment_counts(threshold = 0)
-    # We use includes when we need to prefetch an association and use
-    # those associated records. If we only want to *aggregate* the
+    # We use `includes` when we need to prefetch an association and
+    # use those associated records. If we only want to *aggregate* the
     # associated records somehow, includes is wasteful, because all
     # the associated records are pulled down into the app.
     #
-    # For instance, post_comment_counts just aggregates the number of
-    # comments per post. We don't actually want to keep the post
-    # records.
+    # For instance, if a `User` has posts with many, many comments, we
+    # would pull down every single comment. This may be more rows than
+    # our Rails app can handle. And we don't actually care about all
+    # the individual rows, we just want the count of how many there
+    # are.
     #
-    # You can use `group` to specify a group to perform an aggregation
-    # in:
+    # When we want to do an "aggregation" like summing the number of
+    # records (and don't care about the individual records), we want
+    # to use `joins`.
 
     posts_with_counts = self
       .posts
-      .select("posts.*, COUNT(*) AS comments_count") # more in a sec
-      .joins(:comments)
+      .select("posts.body, COUNT(*) AS comments_count") # more in a sec
+      .joins(:comments) # .joins("LEFT OUTER JOINS comments")
       .group("posts.id") # "comments.post_id" would be equivalent
+    # in SQL:
     #   SELECT posts.*, COUNT(*) AS comments_count
     #     FROM posts
-    #    WHERE posts.author_id = #{self.id}
     #    JOINS comments
     #       ON comments.post_id = posts.id
+    #    WHERE posts.author_id = #{self.id}
     # GROUP BY posts.id
 
     # As we've seen before using `joins` does not change the type of
-    # object returned: this returns `Post` objects. But, `select` lets
-    # us pick out some "bonus fields". Here, I would like to have the
-    # datbase count the comments per post, and store this in a column
-    # named `comments_count`. The magic is that ActiveRecord will give
-    # me access to this column by dynamically adding a new method to
-    # the returned `Post` objects; I can call `#comments_count` on
-    # any, and it will access the value of this column.
+    # object returned: this returns an `Array` of `Post` objects.
+    #
+    # But we do want some extra data about the `Post`: how many
+    # comments were left on it. We can use `select` to pick up some
+    # "bonus fields" and give us access to extra data.
+    #
+    # Here, I would like to have the datbase count the comments per
+    # post, and store this in a column named `comments_count`. The
+    # magic is that ActiveRecord will give me access to this column by
+    # dynamically adding a new method to the returned `Post` objects;
+    # I can call `#comments_count`, and it will access the value of
+    # this column:
+
+    posts_with_counts.map do |post|
+      # `#comments_count` will access the column we `select`ed in the
+      # query.
+      [post.title, post.comments_count]
+    end
   end
 end
