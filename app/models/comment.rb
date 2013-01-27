59c3afaaf3401ff4432eef84de92ac96375d1155
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-26 16:21:33 -0800

    Comment: describe queries in SQL

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,11 +11,22 @@ class Comment < ActiveRecord::Base
   # Rails would look for an `authors` table if we didn't tell it that
   # `author_id` refers to a `User`.
   belongs_to :author, :class_name => "User"
+  # SELECT *
+  #   FROM users
+  #  WHERE users.id = #{self.author_id}
+
   belongs_to :post
+  # SELECT *
+  #   FROM posts
+  #  WHERE posts.id = #{self.post_id}
+
   # Rails would look for `parent_id`, if we didn't give it the foreign
   # key name explicitly. Reminder: a *foreign key* is a database
   # column whose entries are primary keys (ids) in another table.
   belongs_to :parent, :class_name => "Comment", :foreign_key => "parent_comment_id"
+  # SELECT *
+  #   FROM comments
+  #  WHERE comment.id = #{self.parent_comment_id}
 
   def self.reply_to_post(post, user, body)
     Comment.create!(
